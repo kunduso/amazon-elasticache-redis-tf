5e55d0f6d45bf6e071bc7e304ccb37d167910ee8
Sourav Kundu <skundu.dev@gmail.com>
2023-10-15 11:25:40 -0500

    added security group for elasticache

--- security_group.tf
+++ security_group.tf
@@ -1,3 +1,22 @@
 resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.this.id
 }
+resource "aws_security_group" "elasticache" {
+  name        = "app4-elasticache"
+  description = "Allow inbound to and outbound access from the Amazon ElastiCache cluster."
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+    description = "Enable access from an Amazon EC2 instance in the VPC"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Enable access to the ElastiCache cluster."
+  }
+  vpc_id = aws_vpc.this.id
+}
