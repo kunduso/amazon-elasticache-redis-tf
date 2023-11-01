39e0d64fdf229df554c179c2c8b9251b2f54bc4b
Sourav Kundu <skundu.dev@gmail.com>
2023-11-01 12:41:03 -0500

    create ec2 role for instance profile

---
+++ ec2_role.tf
@@ -0,0 +1,41 @@
+# #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
+resource "aws_iam_role" "ec2_role" {
+  name = "app-4-ec2-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+#Attach role to policy
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
+resource "aws_iam_role_policy_attachment" "custom" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachement" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ssm_parameter_policy.arn
+}
+resource "aws_iam_role_policy_attachment" "secret_policy_attachement" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.secret_manager_policy.arn
+}
+#Attach role to an instance profile
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_instance_profile
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "app-4-ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
