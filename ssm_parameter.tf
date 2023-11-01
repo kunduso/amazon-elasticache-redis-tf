a17d09d3e2f907ec569418e7db271ebe0c020e49
Sourav Kundu <skundu.dev@gmail.com>
2023-10-31 23:05:44 -0500

    store endpoint and port as secure parameters

---
+++ ssm_parameter.tf
@@ -0,0 +1,13 @@
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ssm_parameter
+resource "aws_ssm_parameter" "elasticache_ep" {
+  name   = "/elasticache/${aws_elasticache_replication_group.app4.replication_group_id}/endpoint"
+  type   = "SecureString"
+  key_id = aws_kms_key.encrytion_rest.id
+  value  = aws_elasticache_replication_group.app4.configuration_endpoint_address
+}
+resource "aws_ssm_parameter" "elasticache_port" {
+  name   = "/elasticache/${aws_elasticache_replication_group.app4.replication_group_id}/port"
+  type   = "SecureString"
+  key_id = aws_kms_key.encrytion_rest.id
+  value  = aws_elasticache_replication_group.app4.port
+}
