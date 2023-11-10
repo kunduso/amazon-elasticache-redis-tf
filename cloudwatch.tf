74d6c35d0b53532a3e98e951f255754b37d560f4
Sourav Kundu <skundu.dev@gmail.com>
2023-11-09 18:52:30 -0600

    added encryption to log group

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,8 +1,10 @@
 resource "aws_cloudwatch_log_group" "slow_log" {
   name              = "/elasticache/${aws_elasticache_replication_group.app4.replication_group_id}/slow-log"
   retention_in_days = 30
+  kms_key_id        = aws_kms_key.encrytion_rest.id
 }
 resource "aws_cloudwatch_log_group" "engine_log" {
   name              = "/elasticache/${aws_elasticache_replication_group.app4.replication_group_id}/engine-log"
   retention_in_days = 30
+  kms_key_id        = aws_kms_key.encrytion_rest.id
 }
