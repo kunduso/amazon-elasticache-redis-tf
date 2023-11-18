564180b2187e7bc25787a94b32d84fe6c6f67e86
Sourav Kundu <skundu.dev@gmail.com>
2023-11-18 07:51:34 -0600

    aded data block for resource naming

---
+++ data.tf
@@ -0,0 +1,7 @@
+data "aws_caller_identity" "current" {}
+locals {
+  principal_root_arn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+  principal_logs_arn = "logs.${var.region}.amazonaws.com"
+  slow_log_arn = "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:/elasticache/${var.replication_group_id}/slow-log"
+  engine_log_arn = "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:/elasticache/${var.replication_group_id}/engine-log"
+}
