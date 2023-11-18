81177410b9ca52ccbd88c179640cd7c5eb1f10ad
Sourav Kundu <skundu.dev@gmail.com>
2023-11-18 08:17:39 -0600

    enabled encryption

--- ssm_parameter.tf
+++ ssm_parameter.tf
@@ -2,12 +2,12 @@
 resource "aws_ssm_parameter" "elasticache_ep" {
   name = "/elasticache/app-4/${aws_elasticache_replication_group.app4.replication_group_id}/endpoint"
   type = "SecureString"
-  #key_id = aws_kms_key.encryption_rest.id
+  key_id = aws_kms_key.encryption_rest.id
   value = aws_elasticache_replication_group.app4.configuration_endpoint_address
 }
 resource "aws_ssm_parameter" "elasticache_port" {
   name = "/elasticache/app-4/${aws_elasticache_replication_group.app4.replication_group_id}/port"
   type = "SecureString"
-  #key_id = aws_kms_key.encryption_rest.id
+  key_id = aws_kms_key.encryption_rest.id
   value = aws_elasticache_replication_group.app4.port
 }
