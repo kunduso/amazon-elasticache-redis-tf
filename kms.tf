897d9234cebf38d33f2ad765e7b7842b73d39cee
Sourav Kundu <skundu.dev@gmail.com>
2024-11-15 22:54:17 -0600

    added policy to kms key

--- kms.tf
+++ kms.tf
@@ -3,10 +3,63 @@ resource "aws_kms_key" "encryption_secret" {
   enable_key_rotation     = true
   description             = "Key to encrypt secret"
   deletion_window_in_days = 7
-  #checkov:skip=CKV2_AWS_64: Not including a KMS Key policy
+
+  # Attach the KMS key policy
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowSecretsManagerUse"
+        Effect = "Allow"
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*"
+        ]
+        Resource = "*"
+        Principal = {
+          Service = "secretsmanager.amazonaws.com"
+        }
+      },
+      {
+        Sid      = "AllowAdminAccessToKMSKey"
+        Effect   = "Allow"
+        Action   = "kms:*"
+        Resource = "*"
+        Principal = {
+          AWS = "*"
+        }
+      },
+      {
+        Sid    = "AllowIAMRolesToUseKey"
+        Effect = "Allow"
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*"
+        ]
+        Resource = "*"
+        Principal = {
+          AWS = "*"
+        }
+        Condition = {
+          StringEquals = {
+            "kms:CallerAccount" = "${data.aws_caller_identity.current.account_id}"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.name}-encryption-secret"
+  }
 }
+
 resource "aws_kms_alias" "encryption_secret" {
-  name          = "alias/elasticache-app-4-in-transit"
+  name          = "alias/${var.name}-elasticache-in-transit"
   target_key_id = aws_kms_key.encryption_secret.key_id
 }
 resource "aws_kms_key" "encryption_rest" {
@@ -14,15 +67,18 @@ resource "aws_kms_key" "encryption_rest" {
   description             = "Key to encrypt cache at rest."
   deletion_window_in_days = 7
   #checkov:skip=CKV2_AWS_64: KMS Key policy in a separate resource
+  tags = {
+    Name = "${var.name}-encryption-rest"
+  }
 }
 resource "aws_kms_alias" "encryption_rest" {
-  name          = "alias/elasticache-app-4-at-rest"
+  name          = "alias/${var.name}-elasticache-at-rest"
   target_key_id = aws_kms_key.encryption_rest.key_id
 }
 resource "aws_kms_key_policy" "encryption_rest_policy" {
   key_id = aws_kms_key.encryption_rest.id
   policy = jsonencode({
-    Id = "encryption-rest"
+    Id = "${var.name}-encryption-rest"
     Statement = [
       {
         Action = "kms:*"
