e2e47b1494a36795a83d5de66ad35f869d6305cd
Sourav Kundu <skundu.dev@gmail.com>
2024-11-15 22:43:01 -0600

    converted to variable

--- ssm_parameter.tf
+++ ssm_parameter.tf
@@ -1,12 +1,12 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ssm_parameter
 resource "aws_ssm_parameter" "elasticache_ep" {
-  name   = "/elasticache/app-4/${aws_elasticache_replication_group.app4.replication_group_id}/endpoint"
+  name   = "/elasticache/${var.name}/${aws_elasticache_replication_group.app4.replication_group_id}/endpoint"
   type   = "SecureString"
   key_id = aws_kms_key.encryption_rest.id
   value  = aws_elasticache_replication_group.app4.configuration_endpoint_address
 }
 resource "aws_ssm_parameter" "elasticache_port" {
-  name   = "/elasticache/app-4/${aws_elasticache_replication_group.app4.replication_group_id}/port"
+  name   = "/elasticache/${var.name}/${aws_elasticache_replication_group.app4.replication_group_id}/port"
   type   = "SecureString"
   key_id = aws_kms_key.encryption_rest.id
   value  = aws_elasticache_replication_group.app4.port
