5b02323b2d25463fe90593ace53b087302b15c2d
Sourav Kundu <skundu.dev@gmail.com>
2024-11-19 06:51:21 -0600

    separated out sg rules

--- security_group.tf
+++ security_group.tf
@@ -1,22 +1,26 @@
-resource "aws_default_security_group" "default" {
-  vpc_id = aws_vpc.this.id
-}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
 resource "aws_security_group" "elasticache" {
-  name        = "app-4-elasticache-sg"
+  name        = "${var.name}-elasticache-sg"
   description = "Allow inbound to and outbound access from the Amazon ElastiCache cluster."
-  ingress {
-    from_port   = 6379
-    to_port     = 6379
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr]
-    description = "Enable communication to the Amazon ElastiCache for Redis cluster. "
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Enable access to the ElastiCache cluster."
-  }
-  vpc_id = aws_vpc.this.id
+  vpc_id      = module.vpc.vpc.id
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
+resource "aws_security_group_rule" "elasticache_ingress" {
+  type              = "ingress"
+  security_group_id = aws_security_group.elasticache.id
+  from_port         = 6379
+  to_port           = 6379
+  protocol          = "tcp"
+  cidr_blocks       = [var.vpc_cidr]
+  description       = "Enable communication to the Amazon ElastiCache for Redis cluster."
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
+resource "aws_security_group_rule" "elasticache_egress" {
+  type              = "egress"
+  security_group_id = aws_security_group.elasticache.id
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Enable access to the ElastiCache cluster."
 }
